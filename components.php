db9af9dcba1c3df28cd876c12f9c37e684743ac8
Salome <salome@example.com>
2025-10-03 16:53:38 +0400

    courses page

--- components.php
+++ components.php
@@ -8,8 +8,8 @@ function hheadera($headerebi){
             <div class="kvedaheaders">
                 <div class="kvedaheaders1">
                     <img src="' . $headeri['logo'] . '" alt="Logo">
-                    <a href="#"> <h3 class ="pirveladze">' . $headeri['secondtext'] . '</h3> </a>
-                    <a href="#"> <h3>' . $headeri['thirdtext'] . '</h3> </a>
+                    <a href="index.php"> <h3 class ="pirveladze">' . $headeri['secondtext'] . '</h3> </a>
+                    <a href="courses.php"> <h3>' . $headeri['thirdtext'] . '</h3> </a>
                     <a href="#"> <h3>' . $headeri['fourthtext'] . '</h3> </a>
                     <a href="#"> <h3>' . $headeri['fifthtext'] . '</h3> </a>
                     <a href="#"> <h3>' . $headeri['sixthtext'] . '</h3> </a>
@@ -384,3 +384,94 @@ function footerstart($footerinfos) {
 };
 
 // footer end
+
+// 2nd page
+
+function texts($desks){
+    foreach($desks as $desk){
+        echo '<div class="firstpart container" >
+                <div class="fpart"><h2>' . $desk['text1'] . '</h2></div>
+                <div class="spart"><p>' . $desk['text2'] . '</p></div>
+              </div>';
+    }
+};
+
+function main($kartebi){
+    foreach($kartebi as $karti){
+        echo '<div class="mainc container">
+
+            <div class="heading"><h2>' . $karti['text1'] .'</h2>
+            
+                <div class="heading2"><p>' . $karti['text2'] .'</p>
+                
+                    <a href=""> ' . $karti['button'].'</a>
+                
+                
+                </div>
+            
+            </div>
+           
+             
+            <div class="cardz"> 
+    
+                <img src="'.$karti['pic1'].'" alt="">
+                <img src="'.$karti['pic2'].'" alt="">
+                <img src="'.$karti['pic3'].'" alt="">
+            
+            </div>
+        
+            <div class="info"> 
+
+                <h3> '.$karti['date'] .' </h3>
+                <h4> '.$karti['level'] .' </h4>
+                <h5> '.$karti['author'] .' </h5>
+
+        
+            </div>
+        
+            <div class="curi"> 
+
+                <h1> ' . $karti['date2'].'</h1>
+
+            <div class="culum"> 
+
+            <div class="xinkali"> 
+                <h1>'.$karti['1'] . ' </h1>
+                <p>'.$karti['11'] . ' </p>   
+            </div>
+
+            <div class="xinkali"> 
+
+                <h1>'.$karti['2'] . ' </h1>
+                <p>'.$karti['22'] . ' </p>  
+            </div>
+
+            <div class="xinkali"> 
+
+                 <h1>'.$karti['3'] . ' </h1>
+                <p>'.$karti['33'] . ' </p>   
+            </div>
+
+            <div class="xinkali"> 
+
+                <h1>'.$karti['4'] . ' </h1>
+                <p>'.$karti['44'] . ' </p>   
+            </div>
+
+            <div class="xinkali"> 
+
+                <h1>'.$karti['5'] . ' </h1>
+                <p>'.$karti['55'] . ' </p>
+                
+            </div>
+
+            </div>
+            
+            </div>
+            
+
+        </div>';
+    }
+};
+
+// 2nd page END
