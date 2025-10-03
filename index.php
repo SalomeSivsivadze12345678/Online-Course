494d579a6d4f2dd948a519fdfb509bfdb5c3fcf4
LBJ837 <lashalobjanidze2005@gmail.com>
2025-10-03 16:32:36 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
-
-<?php
-
-include ('components.php');
-include ('data.php');
+ <?php 
+include 'data.php';
+include 'components.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;500;700&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About Us - Skillbridge</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+  <main class="about-page"> 
+    <?php 
+      renderAboutIntro($aboutIntro);
+      renderAchievements($achievements);
+      renderGoals($goals);
+      renderCTA($cta);
+    ?>
+  </main>
+
+</body>
+</html>
