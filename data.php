8bd8b6fc8508e808c099d4f7e4fc9cc18dec2b46
SalomeSivsivadze12345678 <ss.sivsivadze@gmail.com>
2025-10-03 14:44:29 +0400

    Add files via upload

---
+++ data.php
@@ -0,0 +1,145 @@
+<?php
+
+$description = [
+
+    [
+        'text1' => 'Online Courses on Design and Development',
+        'text2' => 'Welcome to our online course page, where you can enhance your skills in design and development. Choose from our carefully curated selection of 10 courses designed to provide you with comprehensive knowledge and practical experience. Explore the courses below and find the perfect fit for your learning journey.'
+    ]
+
+
+];
+
+$cards = [
+
+[
+    'text1'=> 'Web Design Fundamentals',
+    'text2'=> 'Learn the fundamentals of web design, including HTML, CSS, and responsive design principles. Develop the skills to create visually appealing and user-friendly websites.',
+    'button' => 'View Course',
+    'pic1' => './assets/Image.svg',
+    'pic2' => './assets/Image (1).svg',
+    'pic3' => './assets/Image (2).svg',
+    'date'=> '4weeks',
+    'level' => 'beginner',
+    'author'=> ' by John Smith',
+    'date2' => 'curriculum',
+    '1'=> '01',
+    '2'=> '02',
+    '3'=> '03',
+    '4'=> '04',
+    '5'=> '05',
+    '11'=> 'Introduction to HTML',
+    '22'=> 'Styling with CSS',
+    '33'=> 'Introduction to Responsive Design',
+    '44'=> 'Design Principles for Web',
+    '55'=> 'Building a Basic Website',
+
+    
+],
+
+
+    [
+    'text1'=> 'UI/UX Design',
+    'text2'=> 'Master the art of creating intuitive user interfaces (UI) and enhancing user experiences (UX). Learn design principles, wireframing, prototyping, and usability testing techniques.',
+    'button' => 'View Course',
+    'pic1' => './assets/11.svg',
+    'pic2' => './assets/22.svg',
+    'pic3' => './assets/33.svg',
+    'date'=> '6 Weeks',
+    'level' => 'Intermediate',
+    'author'=> ' By Emily Johnson',
+    'date2' => 'curriculum',
+    '1'=> '01',
+    '2'=> '02',
+    '3'=> '03',
+    '4'=> '04',
+    '5'=> '05',
+    '11'=> 'Introduction to UI/UX Design',
+    '22'=> 'User Research and Personas',
+    '33'=> 'Wireframing and Prototyping',
+    '44'=> 'Visual Design and Branding',
+    '55'=> 'Usability Testing and Iteration',
+
+    
+    ],
+
+     [
+
+    'text1'=> 'Mobile App Development',
+    'text2'=> 'Dive into the world of mobile app development. Learn to build native iOS and Android applications using industry-leading frameworks like Swift and Kotlin.',
+    'button' => 'View Course',
+    'pic1' => './assets/1.svg',
+    'pic2' => './assets/2.svg',
+    'pic3' => './assets/3.svg',
+    'date'=> '8 Weeks',
+    'level' => 'Intermediate',
+    'author'=> 'By David Brown',
+    'date2' => 'curriculum',
+    '1'=> '01',
+    '2'=> '02',
+    '3'=> '03',
+    '4'=> '04',
+    '5'=> '05',
+    '11'=> 'Introduction to Mobile App Development',
+    '22'=> 'Fundamentals of Swift Programming (iOS)',
+    '33'=> 'Fundamentals of Kotlin Programming (Android)',
+    '44'=> 'Building User Interfaces',
+    '55'=> 'App Deployment and Testing',
+
+    
+     ], 
+    
+    [
+    'text1'=> 'Graphic Design for Beginners',
+    'text2'=> ' Discover the fundamentals of graphic design, including typography, color theory, layout design, and image manipulation techniques. Create visually stunning designs for print and digital media.',
+    'button' => 'View Course',
+    'pic1' => './assets/111.svg',
+    'pic2' => './assets/222.svg',
+    'pic3' => './assets/333.svg',
+    'date'=> '10 Weeks',
+    'level' => 'Beginner',
+    'author'=> ' By Sarah Thompson',
+    'date2' => 'curriculum',
+    '1'=> '01',
+    '2'=> '02',
+    '3'=> '03',
+    '4'=> '04',
+    '5'=> '05',
+    '11'=> 'Introduction to Graphic Design',
+    '22'=> 'Typography and Color Theory',
+    '33'=> 'Layout Design and Composition',
+    '44'=> 'Image Editing and Manipulation',
+    '55'=> 'Designing for Print and Digital Media',
+
+    
+    ],   
+    
+    [
+    'text1'=> 'Front-End Web Development',
+    'text2'=> ' Become proficient in front-end web development. Learn HTML, CSS, JavaScript, and popular frameworks like Bootstrap and React. Build interactive and responsive websites.',
+    'button' => 'View Course',
+    'pic1' => './assets/1111.svg',
+    'pic2' => './assets/2222.svg',
+    'pic3' => './assets/3333.svg',
+    'date'=> '10 Weeks',
+    'level' => 'Intermediate',
+    'author'=> 'By Michael Adams',
+    'date2' => 'curriculum',
+    '1'=> '01',
+    '2'=> '02',
+    '3'=> '03',
+    '4'=> '04',
+    '5'=> '05',
+    '11'=> 'HTML Fundamentals',
+    '22'=> 'CSS Styling and Layouts',
+    '33'=> 'JavaScript Basics',
+    '44'=> 'Building Responsive Websites',
+    '55'=> 'Introduction to Bootstrap and React',
+
+    
+    ], 
+
+
+
+
+];
---
+++ functions.php
@@ -0,0 +1,72 @@
+<?php
+
+function texts($desks){
+    foreach($desks as $desk){
+        echo '<div class="firstpart container" >
+                <div class="fpart"><h2>' . $desk['text1'] . '</h2></div>
+                <div class="spart"><p>' . $desk['text2'] . '</p></div>
+              </div>';
+    }
+};
+
+function main($kartebi){
+    foreach($kartebi as $karti){
+        echo '<div class="mainc container">
+
+            <div class="heading"><h2>' . $karti['text1'] .'</h2>
+            
+                <div class="heading2"><p>' . $karti['text2'] .'</p>
+                
+                    <a href=""> ' . $karti['button'].'</a>
+                
+                
+                </div>
+            
+            </div>
+           
+             
+            <div class="cardz"> 
+    
+                <img src="'.$karti['pic1'].'" alt="">
+                <img src="'.$karti['pic2'].'" alt="">
+                <img src="'.$karti['pic3'].'" alt="">
+            
+            </div>
+        
+            <div class="info"> 
+
+                <h3> '.$karti['date'] .' </h3>
+                <h4> '.$karti['level'] .' </h4>
+                <h5> '.$karti['author'] .' </h5>
+
+        
+            </div>
+        
+            <div class="curi"> 
+
+                <h1> ' . $karti['date2'].'</h1>
+
+            <div class="culum"> 
+
+                <h1>'.$karti['1'] . ' </h1>
+                <p>'.$karti['11'] . ' </p>   
+                <h1>'.$karti['2'] . ' </h1>
+                <p>'.$karti['22'] . ' </p>  
+                 <h1>'.$karti['3'] . ' </h1>
+                <p>'.$karti['33'] . ' </p>   
+                <h1>'.$karti['4'] . ' </h1>
+                <p>'.$karti['44'] . ' </p>   
+                <h1>'.$karti['5'] . ' </h1>
+                <p>'.$karti['55'] . ' </p>
+                
+
+            </div>
+            
+            </div>
+            
+
+        </div>';
+    }
+}
+
+
